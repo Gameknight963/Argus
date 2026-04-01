9eb8e525dc7a619ad6144020a99a5a5ef2c88e5b
sebi <sebisebpaw@gmail.com>
2026-04-01 05:49:26 -0500

    move ArgusConfig class to a separate file

---
+++ ArgusConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Argus
+{
+    public class ArgusConfig
+    {
+        public int RestartThreshold { get; set; } = 3;
+        public int RestartWindowSeconds { get; set; } = 10;
+        public List<string> KillList { get; set; } = new() { "Windhawk" };
+    }
+}
